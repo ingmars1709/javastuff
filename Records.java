92666c2df1f6ccb3f2127d33533669fb45c12304
IngmarS <ingmars@infosupport.com>
2023-02-11 15:32:47 +0100

    eval

--- Records.java
+++ Records.java
@@ -1,15 +1,13 @@
-import java.util.Objects;
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
-
-import static java.util.function.Function.identity;
+import java.util.function.IntUnaryOperator;
 
 public class Records {
     public static void main(String[] args) {
         var r = new Records();
 
         var listOfInts = new Cons<>(5, new Cons<>(3, new Cons<>(2, new Nil<>())));
-        var listOfStrings = new Cons<>("a", new Cons<>("b", new Cons<>("c", new Nil<>())));
 
         System.out.println("sum1 = " + r.sum(listOfInts));
         System.out.println("sum2 = " + r.foldr(listOfInts, Integer::sum, 0));
@@ -17,41 +15,40 @@ public class Records {
         System.out.println("mul1 = " + r.mul(listOfInts));
         System.out.println("mul2 = " + r.foldr(listOfInts, (i1, i2) -> i1 * i2, 1));
 
-        System.out.println("concat1 = " + r.concat(listOfStrings));
-        System.out.println("concat2 = " + r.foldr(listOfStrings, String::concat, ""));
-
         var expr = new Add(
                         new Mul(new Val(3), new Val(4)),
-                        new Val(7));
+                        new Add(new Val(8),
+                                new Mul(new Val(1), new Val(7))));
 
-        Integer calculate = r.eval(expr, Integer::sum, (i1, i2) -> i1 * i2, identity());
-        System.out.println("eval = " + calculate);
+        Integer calculate = r.calc(expr);
+        System.out.println("calculate = " + calculate);
 
-        String prettyPrint = r.eval(expr, (i1, i2) -> i1 + "+" + i2, (i1, i2) -> i1 + "*" + i2, Objects::toString);
-        System.out.println("eval = " + prettyPrint);
+        String prettyPrint = r.prettyprint(expr);
+        System.out.println("pretty print = " + prettyPrint);
+
+        Expr transformed = r.transform(expr, i -> i * 2);
+
+        Integer transformedCalculated = r.calc(transformed);
+        System.out.println("calculate = " + transformedCalculated);
+
+        System.out.println("pretty print = " + r.prettyprint(transformed));
     }
-    Integer sum(final AList<Integer> list) {
+    Integer sum(AList<Integer> list) {
         switch (list) {
             case Cons<Integer> (Integer head, AList<Integer> tail) -> { return head + sum(tail); }
             case Nil<Integer> nil -> { return 0; }
             default -> throw new IllegalStateException();
         }
     }
-    Integer mul(final AList<Integer> list) {
+    Integer mul(AList<Integer> list) {
         switch (list) {
             case Cons<Integer> (Integer head, AList<Integer> tail) -> { return head * mul(tail); }
             case Nil<Integer> nil -> { return 1; }
             default -> throw new IllegalStateException();
         }
     }
-    String concat(final AList<String> list) {
-        switch (list) {
-            case Cons<String> (String head, AList<String> tail) -> { return head + concat(tail); }
-            case Nil<String> nil -> { return ""; }
-            default -> throw new IllegalStateException();
-        }
-    }
-    <T> T foldr(final AList<T> list, BiFunction<T, T, T> f, T base) {
+
+    <T> T foldr(AList<T> list, BiFunction<T, T, T> f, T base) {
         switch (list) {
             case Cons<T> (T head, AList<T> tail) -> { return f.apply(head, foldr(tail, f, base)); }
             case Nil<T> nil -> { return base; }
@@ -59,19 +56,29 @@ public class Records {
             default -> throw new IllegalStateException();
         }
     }
-    <T> T eval(final Expr e, BiFunction<T, T, T> addF, BiFunction<T, T, T> mulF, Function<Integer, T> valF) {
+
+    Integer calc(Expr e) {
         switch (e) {
-            case Add (Expr e1, Expr e2) -> { return addF.apply(eval(e1, addF, mulF, valF), eval(e2, addF, mulF, valF)); }
-            case Mul (Expr e1, Expr e2) -> { return mulF.apply(eval(e1, addF, mulF, valF), eval(e2, addF, mulF, valF)); }
-            case Val v -> { return valF.apply(v.val); }
+            case Add (Expr e1, Expr e2) -> { return calc(e1) + calc(e2); }
+            case Mul (Expr e1, Expr e2) -> { return calc(e1) * calc(e2); }
+            case Val v -> { return (v.val); }
             default -> throw new IllegalStateException();
         }
     }
-    Integer eval(final Expr e) {
+
+    String prettyprint(Expr e) {
         switch (e) {
-            case Add (Expr e1, Expr e2) -> { return eval(e1) + eval(e2); }
-            case Mul (Expr e1, Expr e2) -> { return eval(e1) * eval(e2); }
-            case Val v -> { return (v.val); }
+            case Add (Expr e1, Expr e2) -> { return prettyprint(e1) + "+" + prettyprint(e2); }
+            case Mul (Expr e1, Expr e2) -> { return prettyprint(e1) + "*" + prettyprint(e2); }
+            case Val v -> { return v.val.toString(); }
+            default -> throw new IllegalStateException();
+        }
+    }
+    Expr transform(Expr e, IntUnaryOperator f) {
+        switch (e) {
+            case Add (Expr e1, Expr e2) -> { return new Add(transform(e1, f), transform(e2, f)); }
+            case Mul (Expr e1, Expr e2) -> { return new Mul(transform(e1, f), transform(e2, f)); }
+            case Val v -> { return new Val(f.applyAsInt(v.val)); }
             default -> throw new IllegalStateException();
         }
     }
@@ -82,7 +89,8 @@ public class Records {
     // Specifieke toepassing en krachtig met recursie
     // Mocht je recursieve datastructuren tegenkomen denk dan ook aan recursieve methodes hierop
     // Geen instanceof maar switching en access tot de variabele
-    // foldr als library methode
+    // Stream API is goed in Collections. Dit zijn arbitraire datastructuren
+
 
     sealed interface AList<T> permits Cons, Nil {}
     record Cons<T>(T head, AList<T> tail) implements AList<T> { }
