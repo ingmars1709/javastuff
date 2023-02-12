6b4d5d7d8dcda2eca3f3edb59f7fa08fb576516d
IngmarS <ingmars@infosupport.com>
2023-02-12 17:55:36 +0100

    list projection

--- Records.java
+++ Records.java
@@ -1,63 +1,66 @@
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.function.BiFunction;
 import java.util.function.IntUnaryOperator;
+
+import static java.util.Arrays.asList;
 
 public class Records {
     public static void main(String[] args) {
-        var r = new Records();
+        AList<Integer> listOfInts = create(asList(1,2,3,4,5));
 
-        var listOfInts = new Cons<>(5, new Cons<>(3, new Cons<>(2, new Nil<>())));
+        System.out.println("sum1 = " + sum(listOfInts));
+        System.out.println("mul1 = " + mul(listOfInts));
 
-        System.out.println("sum1 = " + r.sum(listOfInts));
-        System.out.println("sum2 = " + r.foldr(listOfInts, Integer::sum, 0));
+        var expr = new Add(new Mul(new Val(3), new Val(4)),
+                           new Add(new Val(8),
+                                   new Mul(new Val(1), new Val(7)))
+                          );
 
-        System.out.println("mul1 = " + r.mul(listOfInts));
-        System.out.println("mul2 = " + r.foldr(listOfInts, (i1, i2) -> i1 * i2, 1));
-
-        var expr = new Add(
-                        new Mul(new Val(3), new Val(4)),
-                        new Add(new Val(8),
-                                new Mul(new Val(1), new Val(7))));
-
-        Integer calculate = r.calc(expr);
+        Integer calculate = calc(expr);
         System.out.println("calculate = " + calculate);
 
-        String prettyPrint = r.prettyprint(expr);
+        String prettyPrint = prettyprint(expr);
         System.out.println("pretty print = " + prettyPrint);
 
-        Expr transformed = r.transform(expr, i -> i * 2);
+        Expr transformed = transform(expr, i -> i * 2);
 
-        Integer transformedCalculated = r.calc(transformed);
+        Integer transformedCalculated = calc(transformed);
         System.out.println("calculate = " + transformedCalculated);
 
-        System.out.println("pretty print = " + r.prettyprint(transformed));
+        System.out.println("pretty print = " + prettyprint(transformed));
     }
-    Integer sum(AList<Integer> list) {
+
+    static <T> AList<T> create(List<T> list) {
+        return list.isEmpty() ? new Empty<>() : new Cons<T>(list.get(0), create(list.subList(1, list.size())));
+    }
+
+    static Integer sum(AList<Integer> list) {
         switch (list) {
             case Cons<Integer> (Integer head, AList<Integer> tail) -> { return head + sum(tail); }
-            case Nil<Integer> nil -> { return 0; }
+            case Empty<Integer> empty -> { return 0; }
             default -> throw new IllegalStateException();
         }
     }
-    Integer mul(AList<Integer> list) {
+    static Integer mul(AList<Integer> list) {
         switch (list) {
             case Cons<Integer> (Integer head, AList<Integer> tail) -> { return head * mul(tail); }
-            case Nil<Integer> nil -> { return 1; }
+            case Empty<Integer> empty -> { return 1; }
+            default -> throw new IllegalStateException();
+        }
+    }
+    static <T> AList<T> take(Integer n, AList<T> l) {
+        switch (l) {
+            case Cons<T> (T head, AList<T> tail) -> { return n == 0 ? new Empty<T>() : new Cons<T>(head, take (n-1, tail)); }
+            case Empty<T> empty -> { return new Empty<>(); }
             default -> throw new IllegalStateException();
         }
     }
 
-    <T> T foldr(AList<T> list, BiFunction<T, T, T> f, T base) {
-        switch (list) {
-            case Cons<T> (T head, AList<T> tail) -> { return f.apply(head, foldr(tail, f, base)); }
-            case Nil<T> nil -> { return base; }
-            // https://stackoverflow.com/questions/72703351/java-19-pattern-matching-compilation-error-the-switch-statement-does-not-cover
-            default -> throw new IllegalStateException();
-        }
-    }
+    // foldr
+    // perms
+    // comprehensies
 
-    Integer calc(Expr e) {
+
+    static Integer calc(Expr e) {
         switch (e) {
             case Add (Expr e1, Expr e2) -> { return calc(e1) + calc(e2); }
             case Mul (Expr e1, Expr e2) -> { return calc(e1) * calc(e2); }
@@ -65,8 +68,7 @@ public class Records {
             default -> throw new IllegalStateException();
         }
     }
-
-    String prettyprint(Expr e) {
+    static String prettyprint(Expr e) {
         switch (e) {
             case Add (Expr e1, Expr e2) -> { return prettyprint(e1) + "+" + prettyprint(e2); }
             case Mul (Expr e1, Expr e2) -> { return prettyprint(e1) + "*" + prettyprint(e2); }
@@ -74,7 +76,7 @@ public class Records {
             default -> throw new IllegalStateException();
         }
     }
-    Expr transform(Expr e, IntUnaryOperator f) {
+    static Expr transform(Expr e, IntUnaryOperator f) {
         switch (e) {
             case Add (Expr e1, Expr e2) -> { return new Add(transform(e1, f), transform(e2, f)); }
             case Mul (Expr e1, Expr e2) -> { return new Mul(transform(e1, f), transform(e2, f)); }
@@ -83,18 +85,17 @@ public class Records {
         }
     }
 
-    // Deconstructing and recursion in Java 19
+    // Functioneel programmeren in Java. Niet alleen OO is een paradigma
+    // Deconstructing and recursion in Java 19+
     // Haskell patronen vind ik krachtig en hoe staat het nu met Java daarin?
-    // Records in algemeen: https://www.baeldung.com/java-record-vs-lombok
-    // Specifieke toepassing en krachtig met recursie
     // Mocht je recursieve datastructuren tegenkomen denk dan ook aan recursieve methodes hierop
-    // Geen instanceof maar switching en access tot de variabele
-    // Stream API is goed in Collections. Dit zijn arbitraire datastructuren
+    // Stream API is goed in Collections. Dit zijn arbitraire datastructuren en sterk afgeleid van Haskell
+    // Non destructive
+    // pure functions
 
-
-    sealed interface AList<T> permits Cons, Nil {}
+    sealed interface AList<T> permits Cons, Empty {}
     record Cons<T>(T head, AList<T> tail) implements AList<T> { }
-    record Nil<T>() implements AList<T> { }
+    record Empty<T>() implements AList<T> { }
 
     sealed interface Expr permits Add, Mul, Val {}
     record Add(Expr e1, Expr e2) implements Expr {}
